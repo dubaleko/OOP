7a577e8a6d3b99cd0eef5591355fb6e4374f296a
Valentin Dubaleko <dubaleco@yandex.ru>
2018-10-05 12:15:48 +0300

    Сommit 3 lab

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+   partial class Date
+    { 
+     //Поля 
+     private  int day;
+     private  int month;
+     public   int year;
+     private static int numberofobjects = 0;
+     public readonly int ID;
+     //Свойства 
+     public int Day
+       {
+            get
+            {
+                return day;
+            }
+            set
+            {
+                if (value > 1  && value <= 31)
+                {
+                    day = value;
+                }
+                else
+                {
+                    Console.WriteLine("Неверно введенный день");
+                }
+            }
+        }
+       public  int Month
+        {
+            get
+            {
+                return month;
+            }
+            set
+            {
+                if (value > 1 && value <= 12)
+                {
+                    month = value;
+                }
+                else
+                {
+                    Console.WriteLine("Неверно введенный месяц");
+                }   
+            }
+        }
+       public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    year = value;
+                }
+                else
+                {
+                    Console.WriteLine("Неверно введенный год");
+                }
+            }
+        }
+       public  static int NumberofObjects
+        {
+            get
+            {
+                return numberofobjects;
+            }
+        }
+       //Методы
+        public static void Info()
+        {
+            Console.WriteLine($"Из класса Date создано {numberofobjects} экземпляров.");
+        }
+        public static void Vyvod(int day, int month, int year)
+        {
+            if (day > 0 && day <= 31 && month > 0 && month <= 12 && year > 0)
+            {
+                Console.WriteLine($"{day}/{month}/{year}");
+            }
+        }
+        public static void DrygoiVyvod(int day, int month, int year)
+        {
+            if (day > 0 && day <= 31 && month > 0 && month <= 12 && year > 0)
+            {
+                if (month == 1) Console.WriteLine($"{day} января {year} года");
+                if (month == 2) Console.WriteLine($"{day} февраля {year} года");
+                if (month == 3) Console.WriteLine($"{day} марта {year} года");
+                if (month == 4) Console.WriteLine($"{day} апреля {year} года");
+                if (month == 5) Console.WriteLine($"{day} мая {year} года");
+                if (month == 6) Console.WriteLine($"{day} июня {year} года");
+                if (month == 7) Console.WriteLine($"{day} июля {year} года");
+                if (month == 8) Console.WriteLine($"{day} августа {year} года");
+                if (month == 9) Console.WriteLine($"{day} сентября {year} года");
+                if (month == 10) Console.WriteLine($"{day} октября {year} года");
+                if (month == 11) Console.WriteLine($"{day} ноября {year} года");
+                if (month == 12) Console.WriteLine($"{day} декабря {year} года");
+            }
+        }
+        public bool DayEquals(object obj)
+        {
+            Date date = (Date)obj;
+            return this.day == date.day;
+        }
+        public  bool NewEquals(object obj)
+        {
+            Date date = (Date)obj;
+            return this.month == date.month;
+        }
+        public void  Mnozitel(ref int day,ref int month,out int chislo)
+        {
+            chislo = day * month;
+        }
+        //Конструкторы
+        private Date()
+        {
+            Console.WriteLine("Какой-то текст который запихнут в конструктор не обращайте внимание");
+        }
+        static Date() { }
+        public Date(int day, int month, int year)
+        {
+            if (day > 0 && day <= 31)
+            {
+                this.day = day;
+            }
+            if (month > 0 && month <= 12)
+            {
+                this.month = month;
+            }
+            if (year > 0)
+            {
+                this.year = year;
+            }
+            numberofobjects++;
+            ID = GetHashCode();
+        }
+
+        //Переопределение
+        public override int GetHashCode()
+        {
+            return Day.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return $"{day} день {month} месяца {year} года";
+        }
+        public override bool Equals(object obj)
+        {
+            Date date = (Date)obj;
+            return this.year == date.year;
+        }
+
+    }
+}
---
+++ lab3/lab3/Program2.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Programm
+    {
+        static void Main()
+        {
+           Console.WriteLine("Введите день месяц и год");
+           int newday = int.Parse(Console.ReadLine());
+           int newmonth = int.Parse(Console.ReadLine());
+           int newyear = int.Parse(Console.ReadLine());
+           Date FirstDate = new Date(newday,newmonth,newyear);
+           FirstDate.Day = newday;
+           FirstDate.Month = newmonth;
+           FirstDate.Year = newyear;
+           Date.Info();
+           Date.Vyvod(newday, newmonth, newyear);
+           Date.DrygoiVyvod(newday, newmonth, newyear);
+           Console.WriteLine(FirstDate.GetType());
+           Console.WriteLine(FirstDate.ToString());
+           Date SecondDate = new Date(20, 10, 2000);
+           Console.WriteLine(FirstDate.GetHashCode());
+           Console.WriteLine(SecondDate.GetHashCode());
+           Console.WriteLine(FirstDate.Equals(SecondDate));
+           Console.WriteLine("Размер Массива");
+           int razmer = int.Parse(Console.ReadLine());
+           Date[] arr = new Date[razmer];
+           Random rand = new Random();
+            for (int i=0; i < razmer; i++ )
+            {
+                arr[i] = new Date(rand.Next(1, 31), rand.Next(1, 12), rand.Next(1990, 2020));
+            }
+           Console.WriteLine("Даты в массиве");
+           for (int i=0;i<razmer;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+           Console.WriteLine("Введите год который хотите найти в списке");
+           int secondyear = int.Parse(Console.ReadLine());
+           Date takedate = new Date(1, 1, secondyear);
+           Console.WriteLine("Список дат в которых есть заданный год:");
+            int k = 0; 
+           for (int i=0; i<razmer;i++)
+            {
+                if (takedate.Equals(arr[i]) == true)
+                {
+                    Console.WriteLine(arr[i]);
+                    k++;
+                }
+            }
+            if (k == 0)
+            {
+                Console.WriteLine("Дат с этим годом не обнаружено ");
+            }
+            Console.WriteLine("Введите число которое нужно искать");
+            int chislo = int.Parse(Console.ReadLine());
+            Date takeday = new Date(chislo,1,1);
+            Date takemonth = new Date(1, chislo, 1);
+            Date takeyear = new Date(1, 1, chislo);
+            int n = 0;
+            if (chislo > 0 && chislo <= 31)
+            {
+                for (int i = 0; i < razmer; i++)
+                {
+                    if (takeday.DayEquals(arr[i]) == true)
+                    {
+                        Console.WriteLine(arr[i]);
+                        n++;
+                    }
+                }
+            }
+            if (chislo > 0 && chislo <= 12)
+            {
+                for (int i = 0; i < razmer; i++)
+                {
+                    if (takemonth.NewEquals(arr[i]) == true)
+                    {
+                        Console.WriteLine(arr[i]);
+                        n++;
+                    }
+                }
+            }
+            for (int i = 0; i < razmer; i++)
+            {
+                if (takeyear.Equals(arr[i]) == true)
+                {
+                    Console.WriteLine(arr[i]);
+                    n++;
+                }
+            }
+            if (n == 0)
+            {
+                Console.WriteLine("Такого числа в датах нету");
+            }
+            Console.WriteLine("Анонимный тип");
+            var data = new { Day = 10, Month = 10, Year = 2000 };
+            Console.WriteLine($"{data.Day}/{data.Month}/{data.Year}"); 
+
+        }
+    }
+}
