8f481ac3cb39721a40cbc6793e45019e85410b5b
Valentin Dubaleko <dubaleco@yandex.ru>
2018-12-26 20:29:05 +0300

    Last version of 15 lab

---
+++ laba15/laba15/OddEven.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+namespace Lab15
+{
+    static class OddEven
+    {
+        public static void DoItConsistently()
+        {
+            Object locker = new Object();
+            if (File.Exists("OddEvenConsistently.txt"))
+            {
+                File.Delete("OddEvenConsistently.txt");
+            }
+            Thread OddThread = new Thread(new ThreadStart(PrintOdd));
+            Thread EvenThread = new Thread(new ThreadStart(PrintEven));
+            OddThread.Start();
+            EvenThread.Start();
+            void PrintOdd()
+            {
+                lock (locker)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+
+                        if (i % 2 != 0)
+                        {
+                            Console.WriteLine(i + " odd");
+                            WriteResultToFile(i);
+                            Thread.Sleep(250);
+                        }
+                    }
+                }
+
+            }
+            void PrintEven()
+            {
+                lock (locker)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Console.WriteLine(i + " even");
+                            WriteResultToFile(i);
+                            Thread.Sleep(500);
+                        }
+                    }
+                }
+            }
+            void WriteResultToFile(int data)
+            {
+                StreamWriter OddEven = new StreamWriter("OddEvenConsistently.txt", true);
+                OddEven.WriteLine(data);
+                OddEven.Close();
+            }
+        }
+        public static void DoItOneByOne()
+        {
+            Mutex mutex = new Mutex();
+            if (File.Exists("OddEvenOneByOne.txt"))
+            {
+                File.Delete("OddEvenOneByOne.txt");
+            }
+            Thread OddThread = new Thread(new ThreadStart(PrintOdd));
+            Thread EvenThread = new Thread(new ThreadStart(PrintEven));
+            OddThread.Start();
+            EvenThread.Start();
+            void PrintOdd()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    mutex.WaitOne();
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i + " odd");
+                        WriteResultToFile(i);
+                        Thread.Sleep(250);
+                    }
+                    mutex.ReleaseMutex();
+                }
+
+            }
+            void PrintEven()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    mutex.WaitOne();
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i + " even");
+                        WriteResultToFile(i);
+                        Thread.Sleep(500);
+                    }
+                    mutex.ReleaseMutex();
+                }
+
+            }
+            void WriteResultToFile(int data)
+            {
+                StreamWriter OddEven = new StreamWriter("OddEvenOneByOne.txt", true);
+                OddEven.WriteLine(data);
+                OddEven.Close();
+            }
+        }
+    }
+}
--- laba15/laba15/Program.cs
+++ laba15/laba15/Program.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
-using System.IO;
 using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 
-namespace laba15
+namespace Lab15
 {
     class Program
     {
-        static string str = new string('_', 120);
+        static string st = new string('-', 40);
+        static string sw = new string('-', 75);
+        static string str = new string('-', 120);
         private static void SecondaryDomain_DomainUnload(object sender, EventArgs e)
         {
             Console.WriteLine("Domain unloaded from process.");
@@ -22,11 +24,11 @@ namespace laba15
             Console.WriteLine("Build loaded.");
             Console.WriteLine("List of build:");
         }
-        public static void EazyNumb(object x)
+        public static void WriteNums(object x)
         {
             int i = 1;
             int n = (int)x;
-            using (StreamWriter sw = new StreamWriter("aloneThread.txt", false, Encoding.Default))
+            using (StreamWriter sw = new StreamWriter("EazyNumbers.txt"))
             {
                 while (n != i)
                 {
@@ -41,6 +43,7 @@ namespace laba15
                     }
                     if (z == 2)
                     {
+                        Console.WriteLine(i);
                         sw.WriteLine(i);
 
                     }
@@ -48,49 +51,10 @@ namespace laba15
                 }
             }
         }
-        public static void EvenandOddNumb(object x)
-        {
-            int i = 0;
-            List<int> mylist = new List<int>();
-            ValueTuple<int, bool> n = (ValueTuple<int, bool>)x;
-            while (n.Item1 != i)
-                {
-                    if (n.Item2 == false)
-                    {
-                        if (i % 2 == 0)
-                        {
-                        Console.WriteLine(i);
-                        mylist.Add(i);
-                        Thread.Sleep(20);
-                        }
-                    }
-                    if(n.Item2 == true)
-                    {
-                        if (i % 2 != 0)
-                        {
-                        Console.WriteLine(i);
-                        mylist.Add(i);
-                        Thread.Sleep(10);
-                        }
-
-                    }
-                   i++;
-                }
-            using (StreamWriter sw = new StreamWriter("EvenandOddNumb.txt", false, Encoding.Default))
-            {
-                foreach (int c in mylist)
-                {
-                    sw.WriteLine(c.ToString());
-                }
-            }
-        
-        }
         static void Main(string[] args)
         {
-           
-            //get info about process
             var getprocess = Process.GetProcesses();
-            Console.WriteLine(str);
+            Console.WriteLine(st + "Task1" + sw);
             using (StreamWriter sw = new StreamWriter("file.txt", false, Encoding.Default))
             {
                 try
@@ -111,54 +75,72 @@ namespace laba15
                 }
 
             }
-            Console.WriteLine("Process:");
             Console.WriteLine("If you need information about process of 1 task pls check file.txt");
             Console.WriteLine(str);
-            //Domain
-            AppDomain secondaryDomain = AppDomain.CreateDomain("Secondary domain");
-            //build load event
-            secondaryDomain.AssemblyLoad += Domain_AssemblyLoad;
-            //domain unload event
-            secondaryDomain.DomainUnload += SecondaryDomain_DomainUnload;
-            Console.WriteLine($"Domain: {secondaryDomain.FriendlyName}");
-            secondaryDomain.Load(new AssemblyName("System.Data"));
-            Assembly[] assemblies = secondaryDomain.GetAssemblies();
-            foreach (Assembly asm in assemblies)
-                Console.WriteLine(asm.GetName().Name);
-            //unload domain
-            AppDomain.Unload(secondaryDomain);
-            Console.WriteLine(str);
-            //thread with easy numbers
-            int number = 20;
-            Console.WriteLine("Second thread who was working with easy numbers");
-            Thread myThread = new Thread(EazyNumb);
-            myThread.Name = "Second thread";
-            myThread.Start(number);
-            Console.WriteLine("List of easy numbers:");
-            Thread.Sleep(0);
-            using (StreamReader sr = new StreamReader("aloneThread.txt", Encoding.Default))
+
+            Console.WriteLine(st + "Task2" + sw);
+            Console.WriteLine();
+            using (StreamWriter text = new StreamWriter("Domain.txt"))
             {
-                string line;
-                while ((line = sr.ReadLine()) != null)
+                //This app analysis
+                var thisAppDomain = Thread.GetDomain();
+                text.WriteLine($"Name: {thisAppDomain.FriendlyName}");
+                text.WriteLine($"Setup Information: {thisAppDomain.SetupInformation.ToString()}");
+                text.WriteLine("Assemblies:");
+                foreach (var item in thisAppDomain.GetAssemblies())
                 {
-                    Console.WriteLine(line);
+                    text.WriteLine("    " + item.FullName.ToString());
                 }
+                //Domain
+                AppDomain secondaryDomain = AppDomain.CreateDomain("Secondary domain");
+                //build load event
+                secondaryDomain.AssemblyLoad += Domain_AssemblyLoad;
+                //domain unload event
+                secondaryDomain.DomainUnload += SecondaryDomain_DomainUnload;
+                Console.WriteLine($"Domain: {secondaryDomain.FriendlyName}");
+                secondaryDomain.Load(new AssemblyName("System.Data"));
+                Assembly[] assemblies = secondaryDomain.GetAssemblies();
+                foreach (Assembly asm in assemblies)
+                    Console.WriteLine(asm.GetName().Name);
+                //unload domain
+                AppDomain.Unload(secondaryDomain);
+                Console.WriteLine(str);
             }
-            Console.WriteLine("Thread ID:" + myThread.ManagedThreadId.ToString());
-            Console.WriteLine("Name of Thread: " + myThread.Name);
-            //Console.WriteLine("Priority of Thread: " + myThread.Priority.ToString());
-            Console.WriteLine("State of Thread: " + myThread.ThreadState.ToString());
+
+            Console.WriteLine(st + "Task3" + sw);
+            Thread NumbersThread = new Thread(new ParameterizedThreadStart(WriteNums));
+            NumbersThread.Start(10);
+            NumbersThread.Suspend();
+            NumbersThread.Name = "Info about MyThread";
+            Console.WriteLine("Priority: " + NumbersThread.Priority);
+            Thread.Sleep(100);
+            Console.WriteLine("Name: " + NumbersThread.Name);
+            Thread.Sleep(100);
+            Console.WriteLine("ID: " + NumbersThread.ManagedThreadId);
+            Console.WriteLine("List of eazy numbers:");
+            Thread.Sleep(1000);
+            NumbersThread.Resume();
+            Thread.Sleep(1000);
             Console.WriteLine(str);
-            myThread.Abort();
-            //two thread
-            ValueTuple<int, bool> oddnum = (number, true);
-            ValueTuple<int, bool> evennum = (number, false);
-            Console.WriteLine("Working with 2 Thread");
-            Thread evenThread = new Thread(EvenandOddNumb);
-            Thread oddThread = new Thread(EvenandOddNumb);
-            oddThread.Priority = ThreadPriority.AboveNormal;
-            evenThread.Start(evennum);
-            oddThread.Start(oddnum);
+
+            Console.WriteLine(st + "Task4" + sw);
+            Console.WriteLine("Consistently:");
+            OddEven.DoItConsistently();
+            Thread.Sleep(4000);
+            Console.WriteLine("One by one:");
+            OddEven.DoItOneByOne();
+            Thread.Sleep(4000);
+            Console.WriteLine(str);
+
+            Console.WriteLine(st + "Task5" + sw);
+            TimerCallback timerCallback = new TimerCallback(WhatTimeIsIt);
+            Timer timer = new Timer(timerCallback, null, 0, 1000);
+            Thread.Sleep(5000);
+            void WhatTimeIsIt(object obj)
+            {
+                Console.WriteLine($"It's {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}");
+            }
+            Console.WriteLine(str);
         }
     }
 }
